a0070ac6b30a87c2062c3eb39568d250526ceb22
Emdadul Haque <rafathaque1997@gmail.com>
2023-04-23 17:33:36 +0600

    chore: added project

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,7 +1,1 @@
-export default ({ env }) => ({
-  // enable a plugin that doesn't require any configuration
-  i18n: true,
-  "entity-relationship-chart": {
-    enabled: false,
-  }
-});
+export default ({ env }) => ({});
