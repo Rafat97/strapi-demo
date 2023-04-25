38b2f35baa419b7bfb8c9f0eea959b78ca2469cf
Emdadul Haque <rafathaque1997@gmail.com>
2023-04-25 14:06:46 +0600

    chore: add plugin config

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,29 @@
-export default ({ env }) => ({});
+export default ({ env }) => ({
+  "rest-cache": {
+    config: {
+      provider: {
+        name: "memory",
+        getTimeout: 500,
+        options: {
+          // The maximum size of the cache
+          max: 32767,
+          // Update to the current time whenever it is retrieved from cache, causing it to not expire
+          updateAgeOnGet: false,
+        },
+      },
+      strategy: {
+        debug: true,
+        enableEtag: true,
+        hitpass: false,
+        logs: true,
+        contentTypes: [
+          {
+            contentType: "api::about.about",
+            hitpass: false,
+            maxAge: 18000
+          },
+        ],
+      },
+    },
+  },
+});
